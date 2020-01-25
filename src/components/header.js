76939a10b3ab930b612216847391058827e6d028
ramiz-rahman <ramiz.rahman@outlook.com>
2020-01-26 01:20:03 +0600

    feat: change order of links in header

--- src/components/header.js
+++ src/components/header.js
@@ -11,10 +11,10 @@ const Header = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/blog">Blog</Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link to="/about">About</Link>
           </li>
           <li>
             <Link to="/contact">Contact Me</Link>
