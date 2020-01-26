042eee446bc90820dec067363ebb64ba78449eb6
ramiz-rahman <ramiz.rahman@outlook.com>
2020-01-26 15:56:12 +0600

    feat: add Author metadata to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
+  const author = data.site.siteMetadata.author
   return (
     <footer>
-      <p>Created by Ramiz Rahman, © 2019</p>
+      <p>Created by {author}, © 2019</p>
     </footer>
   )
 }
